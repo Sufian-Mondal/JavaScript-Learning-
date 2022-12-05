f553e9bc638228a8d42f9a66a09d2be03c5be015
Abu Sufian <sufianmondal.eee@gmail.com>
2022-12-05 23:10:40 +0600

    lesson one start

---
+++ script.js
@@ -0,0 +1,62 @@
+/*
+let countryName = "Bangladesh";
+let countryPopulation = ("10 cr");
+let capitalCity = "dhaka";
+console.log(countryName);
+console.log(countryPopulation);
+console.log(capitalCity);
+*/
+
+//data type learning 
+/*
+let fristName = "shimul";
+console.log(fristName);
+console.log(fristName);
+console.log(fristName);
+
+
+let martuza_shimul = "MS";
+let $function = 27;
+
+let person = "shimul";
+let PI =3.1416;
+
+let myFristJob ="coder";
+let myCurrentJob = "devloper";
+
+console.log(myFristJob);
+console.log(myCurrentJob);
+
+//most use data type example
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "shimul");
+
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+//undefined examples
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1994
+console.log(typeof "year");
+
+console.log(typeof null);
+*/
+
+
+//variable declarations
+
+let age = 30;
+age = 31;
+
+const birthYear = 1994;
+birthYear = 1993;
