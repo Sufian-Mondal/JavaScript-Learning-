e44b1e99604f03ef6d6b74e28b59c97b4dc8ccd9
Abu Sufian <sufianmondal.eee@gmail.com>
2022-12-11 01:39:26 +0600

    learning 16 class

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ year = 1994
 console.log(typeof "year");
 
 console.log(typeof null);
-*/
+
 
 
 //variable declarations
@@ -60,3 +60,81 @@ age = 31;
 
 const birthYear = 1994;
 birthYear = 1993;
+*/
+
+//mathmatical operator
+/*
+const now = 2037;
+const ageJonas = now - 1994;
+const ageSarah = now-2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10.2 ** 3);//2** means 2 to the power 3
+
+const fristName ="jonas";
+const lastName = "bappy";
+console.log(fristName +' '+ lastName);
+
+//math operator
+
+let x = 10+5;
+x+= 10;
+console.log(x);
+x *= 4;
+console.log(x);
+x++;
+console.log(x);
+x--;
+console.log(x);
+
+
+//comparison Operator
+const ageSarah = 18;
+console.log(now-1991 > now-2018);
+
+
+//operator presidence
+
+const now = 2037;
+const ageJonas = now-1991;
+const ageSarah = now-2018;
+console.log(now-1991>now-2018);
+
+let x,y;
+x=y=25-10-5;//x=y=10;x=10
+console.log(x,y);
+
+const averageAge =(ageJonas + ageSarah)/2;
+console.log(ageJonas, ageSarah, averageAge);
+
+
+const massMark = 78;
+const markHeight = 1.69;
+const massJohn = 95;
+const johnHeight = 1.76;
+
+const BMImark = massMark / markHeight**2;
+const BMIjohn = massJohn / (johnHeight * johnHeight);
+const markHigherBMI = BMImark > BMIjohn;
+
+
+console.log(BMImark, BMIjohn, markHigherBMI);
+
+*/
+
+const fristName = "jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + fristName + ' , a ' + (year-birthYear)+ " years old " + job +"!";
+console.log(jonas);
+
+console.log('just a regular string');
+console.log('string with \n\
+multiple \n\
+action');
+
+
+
+
